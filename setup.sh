74be13c497126a0a5cc02b0781a239284e4e71d3
BRF321JOJO <brandonrf01@gmail.com>
2021-09-05 00:27:40 -0700

    Improved bash scripts

--- setup.sh
+++ setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+#
 # Prepare codebase for fuzzing
+
 set -e
 
-if ! [ -p a2j ]; then
+if ! [[ -p a2j ]]; then
   mkfifo a2j
 fi
 
-if ! [ -p j2a ]; then
+if ! [[ -p j2a ]]; then
   mkfifo j2a
 fi
 
-if ! [ -d seeds ]; then
+if ! [[ -d seeds ]]; then
   mkdir seeds
 fi
 
