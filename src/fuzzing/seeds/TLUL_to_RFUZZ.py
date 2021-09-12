24e11684a5e398d2cc85b2f0d32ad3e4136e0828
BRF321JOJO <brandonrf01@gmail.com>
2021-09-12 09:02:24 -0700

    Improved conversion script by adding padding bytes from RFUZZ harness

--- src/fuzzing/seeds/TLUL_to_RFUZZ.py
+++ src/fuzzing/seeds/TLUL_to_RFUZZ.py
@@ -2,7 +2,7 @@
 import sys
 from ast import literal_eval as make_tuple
 
-# (name, size, value)
+# TODO: 21 is generated for the pop function defined in the RFUZZ harness. Please generalize 21 as such.
 def main(input_file):
     with open(input_file, "r") as file:
         input_data = file.read()
@@ -13,23 +13,25 @@ def main(input_file):
 
     cumulative_size = 0
     binary = 0
-    for cycle in parsed_instructions:
+    for i, cycle in enumerate(parsed_instructions):
         for instruction in cycle:
             _, size, value = instruction
             mask = (1 << size) - 1
             binary = ((value & mask) << cumulative_size) | binary
             cumulative_size += size
-
-    def ceil_8(num):
-        remainder = num % 8
-        if remainder:
-            num += 8 - remainder
-        return num // 8
+        cumulative_size = 21*8*(i+1)
 
     output_file = "binary/rfuzz_test.hwf"
     with open(output_file, "wb") as file:
-        print(binary.to_bytes(ceil_8(cumulative_size), byteorder='big'))
-        file.write(binary.to_bytes(ceil_8(cumulative_size), byteorder='big'))
+        print(binary.to_bytes(21*len(cycles), byteorder='big'))
+        file.write(binary.to_bytes(21*len(cycles), byteorder='big'))
+
+def ceil_8(num):
+    remainder = num % 8
+    if remainder:
+        num += 8 - remainder
+    return num // 8
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
