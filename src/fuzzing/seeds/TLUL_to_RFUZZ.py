3c9fb901e67fac9c0ad98a9e9a38d72bff5c096f
BRF321JOJO <brandonrf01@gmail.com>
2021-09-13 19:22:49 -0700

    In progress: Updated TLUL_to_RFUZZ script to account for ordering of popped bytes

--- src/fuzzing/seeds/TLUL_to_RFUZZ.py
+++ src/fuzzing/seeds/TLUL_to_RFUZZ.py
@@ -3,23 +3,33 @@ import sys
 from ast import literal_eval as make_tuple
 
 # TODO: 21 is generated for the pop function defined in the RFUZZ harness. Please generalize 21 as such.
+bytesPerCycle = 21
+
 def main(input_file):
     with open(input_file, "r") as file:
         input_data = file.read()
 
     cycles = input_data.split('---')
-    instructions = [cycle.split('\n') for cycle in cycles]
-    parsed_instructions = [[make_tuple(inst) for inst in cycle if inst] for cycle in instructions]
+    allInsts = [cycle.split('\n') for cycle in cycles]
+    parsed_instructions = [[make_tuple(inst) for inst in cycle if inst] for cycle in allInsts]
 
     cumulative_size = 0
     binary = 0
     for i, cycle in enumerate(parsed_instructions):
-        for instruction in cycle:
-            _, size, value = instruction
+        for inst in cycle:
+            _, size, value = inst
             mask = (1 << size) - 1
             binary = ((value & mask) << cumulative_size) | binary
             cumulative_size += size
-        cumulative_size = 21*8*(i+1)
+        cumulative_size = bytesPerCycle*8*(i+1)
+
+    zero_padding = cumulative_size - len(bin(binary)[2:])
+    full_value = "0"*zero_padding + bin(binary)[2:]
+    binary = [full_value[i:i+8] for i in range(0, len(full_value), 8)]
+    binary.reverse()
+    binary = ''.join(binary)
+    binary = int(binary, 2)
+
 
     output_file = "binary/rfuzz_test.hwf"
     with open(output_file, "wb") as file:
